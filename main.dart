37acaa70626dd71b980edd3eef8500e39a4b061d
1513kumar <118712765+1513kumar@users.noreply.github.com>
2023-03-18 14:23:48 +0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,3 +1,4 @@
+// Group 3; Sachal Raja _ 19k-1478, Om Kumar _ 19k-0326, Aisha Naoman _ 19k-1362 
 import 'package:flutter/material.dart';
 import 'package:flutter_adaptive_layouts/MasterDetailPage.dart';
 
